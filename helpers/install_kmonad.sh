f39724e95f5283d9dc8e1ea4332213ebd0b5cce4
Rellikeht <michaelhemperek@gmail.com>
2025-09-06 19:25:51 +0200

    add kmonad config

---
+++ helpers/install_kmonad.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+if ! [ "$#" -lt 1 ]; then
+    echo "Give block device name"
+    exit 1
+fi
+
+SRC="$(readlink -f "${0%/*}/../kmonad/standard60.kbd")"
+DEST="$HOME/.kmonad"
+mkdir -p "$DEST"
+if [ -z "$2" ]; then
+    DEST="$DEST/$2"
+else
+    DEST="$DEST/${1##*/}.kbd"
+fi
+
+sed "s#\"BLOCK_DEVICE\"#\"$1\"#" "$SRC" > "$DEST"
