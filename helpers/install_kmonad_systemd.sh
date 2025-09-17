c0fad8b043844a1774842bd1b9797991c89f098d
Rellikeht <michaelhemperek@gmail.com>
2025-09-17 11:14:01 +0200

    fix scripts and add neovide config

--- helpers/install_kmonad_systemd.sh
+++ helpers/install_kmonad_systemd.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 if [ "$#" -lt 1 ]; then
-  echo Give block device name
-  exit 1
+    echo "Give block device name"
+    exit 1
 fi
 
 DNAME="${1##*/}"
@@ -11,31 +11,31 @@ KBD="$HOME/.kmonad/$DNAME.kbd"
 FILENAME="$(systemd-escape -p --suffix=service "$DNAME" 2>/dev/null)"
 TEMP=$(mktemp -d)
 clean() {
-  rm -fr "$TEMP"
+    rm -fr "$TEMP"
 }
 trap clean EXIT
 
 cd "$TEMP"
 {
-  echo "[Unit]"
-  echo "Description=Kmonad instance for $1"
-  echo "After=network-online.target sockets.target"
-  echo "Wants=network-online.target sockets.target"
-  echo
-  echo "[Service]"
-  echo "ExecStart=bash -c 'PATH=\"\$PATH:%h/.nix-profile/bin:\" kmonad $KBD'"
-  echo "Type=simple"
-  echo "Restart=always"
-  echo "RestartSec=2"
-  echo
-  echo "[Install]"
-  echo "WantedBy=default.target"
+    echo "[Unit]"
+    echo "Description=Kmonad instance for $1"
+    echo "After=network-online.target sockets.target"
+    echo "Wants=network-online.target sockets.target"
+    echo
+    echo "[Service]"
+    echo "ExecStart=bash -c 'PATH=\"\$PATH:%h/.nix-profile/bin:\" kmonad $KBD'"
+    echo "Type=simple"
+    echo "Restart=always"
+    echo "RestartSec=2"
+    echo
+    echo "[Install]"
+    echo "WantedBy=default.target"
 } > "$FILENAME"
 cd - >/dev/null
 
 if [ -n "$NO_INSTALL" ]; then
-  cat "$TEMP/$FILENAME"
-  exit 0
+    cat "$TEMP/$FILENAME"
+    exit 0
 fi
 mkdir -p "$DEST"
 cp "$TEMP/$FILENAME" "$DEST"
--- helpers/install_sshfs_systemd.sh
+++ helpers/install_sshfs_systemd.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env sh
 
 if [ "$#" -lt 2 ]; then
-  echo Give mount point and remote source
-  exit 1
+    echo "Give mount point and remote source"
+    exit 1
 fi
 
 DEST="$HOME/.config/systemd/user"
 FILENAME="$(systemd-escape -p --suffix=mount "$2")"
 TEMP=$(mktemp -d)
 clean() {
-  rm -fr "$TEMP"
+    rm -fr "$TEMP"
 }
 trap clean EXIT
 
 cd "$TEMP"
 {
-  echo "[Unit]"
-  echo "Description=Mount sshfs from $1 on $2"
-  echo "After=network-online.target sockets.target"
-  echo "Wants=network-online.target sockets.target"
-  echo
-  echo "[Mount]"
-  echo "What=$1"
-  echo "Where=$2"
-  echo "Type=sshfs"
-  echo "Options=auto_cache,reconnect"
-  echo
-  echo "[Install]"
-  echo "WantedBy=default.target"
+    echo "[Unit]"
+    echo "Description=Mount sshfs from $1 on $2"
+    echo "After=network-online.target sockets.target"
+    echo "Wants=network-online.target sockets.target"
+    echo
+    echo "[Mount]"
+    echo "What=$1"
+    echo "Where=$2"
+    echo "Type=sshfs"
+    echo "Options=auto_cache,reconnect"
+    echo
+    echo "[Install]"
+    echo "WantedBy=default.target"
 } > "$FILENAME"
 cd - >/dev/null
 
 if [ -n "$NO_INSTALL" ]; then
-  cat "$TEMP/$FILENAME"
-  exit 0
+    cat "$TEMP/$FILENAME"
+    exit 0
 fi
 mkdir -p "$DEST"
 cp "$TEMP/$FILENAME" "$DEST"
